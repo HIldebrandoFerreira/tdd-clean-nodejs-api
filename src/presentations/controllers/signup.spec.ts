0f6bac0d01ff5ae013852cde6dc117ddb3949478
Hildebrando Ferreira do Nascimento <hildebrandoweb@gmail.com>
2021-03-06 20:42:12 -0300

    test: levatar erro com jest para validar exception

--- src/presentations/controllers/signup.spec.ts
+++ src/presentations/controllers/signup.spec.ts
@@ -16,15 +16,6 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      throw new Error()
-    }
-  }
-  return new EmailValidatorStub()
-}
-
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
@@ -117,8 +108,10 @@ describe('SigNup Controller', () => {
     expect(isValidSpy).toHaveBeenCalledWith('any-email@mail.com')
   })
   test('Deve dar erro 500 ao levantar exception em EmailValidator', () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
     const httpRequest = {
       body: {
         name: 'any-name',
