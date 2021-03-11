903b92d0564ed5d7812da95b24c6810271c76823
Hildebrando Ferreira do Nascimento <hildebrandoweb@gmail.com>
2021-03-10 21:31:33 -0300

    feat: garantir que ao chamar o mongo vai dar sucesso se passar os parametros

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,26 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect () {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWhithoutId } = collection
+    return Object.assign({}, collectionWhithoutId, { id: _id })
+  }
+
+}
